46de875bff8379cc588e07373cea531eb30dc631
ckp-0217 <ckp0217@foxmail.com>
2023-02-17 17:56:44 +0800

    service增加交易监控 以及交易获取 事件处理

---
+++ src/main/java/com/example/web3/service/BlockInfoProcessorService.java
@@ -0,0 +1,57 @@
+package com.example.web3.service;
+
+import java.util.List;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.core.methods.response.*;
+import org.web3j.protocol.core.methods.response.EthBlock.TransactionResult;
+import org.web3j.protocol.core.methods.response.Transaction;
+import com.example.web3.model.Rpc;
+import com.example.web3.util.Constant;
+
+
+public class BlockInfoProcessorService {
+    // private static final Logger log = LoggerFactory.getLogger(ScanTx.class);
+
+    static Rpc web3j = Constant.HTTPRPC;
+    static Credentials wallet = Constant.WALLET;
+    static String hash = Constant.TXHASH;
+    static TransactionProcessorService transactionProcessorService=new TransactionProcessorService();
+
+
+
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+        web3j.blockFlowable(false).subscribe(block -> {
+            List<TransactionResult> transactions = block.getBlock().getTransactions();
+            System.out.println("block number :" + block.getBlock().getNumber());
+            for (int i = 0; i < transactions.size(); i++) {
+                TransactionResult transactionResult = transactions.get(i);
+                String transactionHash = (String) transactionResult.get();
+                transactionProcessorService.processTransactionByHash(transactionHash);
+
+
+
+
+
+                //获取到了交易hash
+                // EthGetTransactionReceipt receipt = web3j.ethGetTransactionReceipt(transactionHash).send();
+                // TransactionReceipt transactionReceipt = receipt.getTransactionReceipt().get();
+                // 处理TransactionReceipt
+
+
+                // List<Log> logs = transactionReceipt.getLogs();
+                // for (int j = 0; j < logs.size(); j++) {
+                //     Log log = logs.get(j);
+                //     // 处理日志
+                //     System.out.println(log);
+                // }
+                // System.out.println(transactionReceipt.toString());
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/example/web3/service/TransactionProcessorService.java
@@ -0,0 +1,39 @@
+package com.example.web3.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
+import org.web3j.protocol.core.methods.response.Log;
+import org.web3j.protocol.core.methods.response.Transaction;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+
+import com.example.web3.model.Rpc;
+import com.example.web3.util.Constant;
+
+public class TransactionProcessorService {
+    static Rpc web3j = Constant.HTTPRPC;
+
+    public void processTransaction(Transaction tx) throws IOException {
+        // 获取交易receipt
+        EthGetTransactionReceipt receipt = web3j.ethGetTransactionReceipt(tx.getHash()).send();
+        TransactionReceipt transactionReceipt = receipt.getTransactionReceipt().get();
+        // 处理TransactionReceipt;
+        if (null == transactionReceipt) {
+            return;
+        }
+
+        List<Log> logs = transactionReceipt.getLogs();
+        for (int j = 0; j < logs.size(); j++) {
+            Log log = logs.get(j);
+            // 处理日志
+            System.out.println("tx : " + j + "index: " + log.getLogIndex() + " address: " + log.getAddress()
+                    + " topic: " + log.getTopics() + " data: " + log.getData());
+        }
+    }
+
+    public void processTransactionByHash(String hash) throws IOException {
+        processTransaction(web3j.ethGetTransactionByHash(hash).send().getResult());
+    }
+
+}
